28edac561303bfcbc4a31209ae4d6d43544d8a3a
Alexis Adonis <alexis@alexiss-mbp.mynet>
2019-06-10 16:16:19 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,7 +3,31 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards [0];
+function checkForMatch(){
+
+if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You've found a match!");
+	}
+	else if (cardsInPlay[0] !== cardsInPlay [1]){
+		alert("No match, better luck next time!");
+	}
+
+}
+
+function flipCard(cardId){
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}	
+}
+
+flipCard(0);
+flipCard(2);
+
+
+/*var cardOne = cards [0];
 
 var cardTwo = cards [1];
 
@@ -11,15 +35,12 @@ cardsInPlay.push(cardOne);
 
 cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You've found a match!");
-	}
-	else if (cardsInPlay[0] !== cardsInPlay [1]) {
-		alert("No match, better luck next time!");
-	}
-}
+
+*/
+
+
+
 
 	
 
